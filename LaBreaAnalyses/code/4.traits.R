60efc2fba2b8b9ae267303c9a75e9608bc59182e
jessicablois <jblois@ucmerced.edu>
2020-05-28 17:40:43 -0700

    Updated trait matching file and analyses
    Created new trait file that:
    dealt with sylvilagus sp NISP issue
    pulled out taxa for each assembalge in the sensitivity analysis
    matched the bioclim and pantheria trait data
    
    then calucated the weighted assemblage mean for each trait, for each box.

--- LaBreaAnalyses/code/4.traits.R
+++ LaBreaAnalyses/code/4.traits.R
@@ -1,46 +1,114 @@
 library(tidyverse)
 library(scales)
 
-# read in master mammal_nisp and trait data ----
+
+# read in master file that stores names/assemblages for trait analyses
+dat <- read.delim("data/processed/Master_trait_taxon_names.txt", sep="\t", stringsAsFactors = F)
+
+# read in master mammal_nisp and process to account for Sylvilagus ----
+
 mammals_nisp <- read.delim("data/processed/mammal_nisp.txt", sep="\t", stringsAsFactors = F)
 mammals_nisp[is.na(mammals_nisp)] <- 0 # convert the NAs to 0
+colsForBoxes <- grep("Box", colnames(mammals_nisp)) 
 
-taxonomy_traits <- read.delim("data/processed/TaxonomyTraits_Reg_est_Bioclim.txt", sep="\t", stringsAsFactors = F)  # TO DO FOR NEXT TIME: Create this file through coding rather than assembling de novo - pull trait data from Pantheria and the newly generated bioclim summary stats files, need to match names from bioclim files back to master taxonomy.
+# Estimate Sylvilagus spp contributions and add onto NISP for the named species ----
 
-# merge trait data with nisp ----
-dat <- cbind(mammals_nisp, taxonomy_traits[match(mammals_nisp$RevisedName, taxonomy_traits$RevisedName),-c(1:5)])
-colsForBoxes <- grep("Box", colnames(dat)) 
-
-# Estimate Sylvilagus spp contributions ----
 # calculate Sylvilagus ratios and determin addition factors
-temp_bach <- dat[which(dat$RevisedName == "Sylvilagus bachmani"),]
-temp_audu <- dat[which(dat$RevisedName == "Sylvilagus audubonii"),]
+temp_bach <- mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus bachmani"),]
+temp_audu <- mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus audubonii"),]
 temp_df <- colSums(rbind(temp_bach, temp_audu)[,colsForBoxes], na.rm=T)
 prop_bach <- temp_bach[,colsForBoxes]/temp_df
 prop_audu <- temp_audu[,colsForBoxes]/temp_df
 
-temp_sylv_sp <- dat[which(dat$RevisedName == "Sylvilagus sp"),]
-total_new_bach <- round(prop_bach*temp_sylv_sp[,colsForBoxes],0)
-total_new_audu <- round(prop_audu*temp_sylv_sp[,colsForBoxes],0) 
+temp_sylv_sp <- mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus sp"),4:7]
+total_new_bach <- round(prop_bach*temp_sylv_sp,0)
+total_new_audu <- round(prop_audu*temp_sylv_sp,0) 
 
 # Add on these genus Sylvilagus proportions to individual species then delete the Sylvilagus sp. row
-dat[which(dat$RevisedName == "Sylvilagus bachmani"),colsForBoxes] <- dat[which(dat$RevisedName == "Sylvilagus bachmani"),colsForBoxes] + total_new_bach
-dat[which(dat$RevisedName == "Sylvilagus audubonii"),colsForBoxes] <- dat[which(dat$RevisedName == "Sylvilagus audubonii"),colsForBoxes] + total_new_audu
-dat <- dat[-which(dat$RevisedName == "Sylvilagus sp"),]
+mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus bachmani"),colsForBoxes] <- mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus bachmani"),colsForBoxes] + total_new_bach
+mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus audubonii"),colsForBoxes] <- mammals_nisp[which(mammals_nisp$RevisedName == "Sylvilagus audubonii"),colsForBoxes] + total_new_audu
+mammals_nisp <- mammals_nisp[-which(mammals_nisp$RevisedName == "Sylvilagus sp"),]
 
-# calculate weighted trait averages ----
 
-# for each trait, first remove the taxa with no trait data
-traits <- c("BodyMass_Mean", "Precip_Mean", "Temp_Mean", "NDVI_Mean", "Temp_SD",	"Temp_max",	"Temp_min"
-)
-trait_weighted_mean <- as.data.frame(matrix(NA, nrow=length(traits), ncol=length(colsForBoxes)))
-colnames(trait_weighted_mean) <- colnames(dat)[colsForBoxes]
-rownames(trait_weighted_mean) <- traits
+# set the scales for the sensitivity analysis
+tax_scales <- c("known", "cf", "estimated")
+spat_scales <- c("local", "regional", "continental")
 
-for (i in 1:length(traits)){
-  dat_temp <- dat[-which(is.na(dat[,traits[i]])),]
-  trait_nisp <- colSums(dat_temp[,colsForBoxes])
-  trait_weighted_mean[i,] <- colSums(dat_temp[,traits[i]]*dat_temp[,colsForBoxes])/trait_nisp
+# create the final trait files for each assemblage
+for (i in 1:length(tax_scales)){
+  for (j in 1:length(spat_scales)){
+    
+    # determine the final taxon list
+    if (i == 1){tax_rows <- which(dat$BestTaxData == tax_scales[i])}
+    if (i == 2){tax_rows <- c(which(dat$BestTaxData == tax_scales[1]), which(dat$BestTaxData == tax_scales[i]))}
+    if (i == 3){tax_rows <- seq(1:nrow(dat))}
+   
+    if (j == 1){spat_rows <- which(dat$BestSpatData == spat_scales[j])}
+    if (j == 2){spat_rows <- c(which(dat$BestSpatData == spat_scales[1]), which(dat$BestSpatData == spat_scales[j]))}
+    if (j == 3){spat_rows <- seq(1:nrow(dat))}
+    
+    taxon_list <- unique(dat$Species[intersect(tax_rows, spat_rows)])
+    taxon_list <- taxon_list[which(!is.na(taxon_list))]
+    taxon_list <- taxon_list[which(dat$include[match(taxon_list, dat$Species)]=="y")]
+
+    # now that we have a taxon list, create a blank dataframe to store the trait data
+    trait_dat <- as.data.frame(matrix(nrow=length(taxon_list), ncol=49))
+    
+    #we want to associate the bioclim data for each species
+    for (s in 1:length(taxon_list)){
+    
+    # read in bioclim summary file, arrange, and store in trait dat
+      bioclim <- read.csv(paste0("data/processed/range_wide_bioclim_stats/range_wide_bioclim_stats_", taxon_list[s], ".csv"), header=T)
+      colnames(bioclim)[1] <- "statistic"
+      bioclim_long <- reshape(bioclim, idvar = "statistic", varying=colnames(bioclim)[2:ncol(bioclim)], direction = "long")
+      colnames(bioclim_long)[2:3] <- c("bioclim_variable", "value")    
+      bioclim_long$bioclim_variable <- gsub("1_5m_", "", bioclim_long$bioclim_variable)  
+      bioclim_colnames <- gsub(".1_5m", "", rownames(bioclim_long))
+      
+      trait_dat[s, 1:length(bioclim_colnames)]<- bioclim_long$value
+      colnames(trait_dat)[1:length(bioclim_colnames)] <- bioclim_colnames
+      
+      }
+
+    # read in Pantheria trait data and store
+    pan <- read.delim("data/raw/PanTHERIA_1-0_WR05_Aug2008.txt", sep="\t", header=T)
+    
+    # pull out PantheriaID for each taxon in taxon list
+    pantheria_list <- dat$PantheriaID[match(taxon_list, dat$Species)]
+    trait_dat[,ncol(trait_dat)] <- pan[match(pantheria_list, pan$MSW05_Binomial),'X5.1_AdultBodyMass_g']
+    colnames(trait_dat)[ncol(trait_dat)] <- 'AdultBodyMass_g'
+    trait_dat <- cbind(taxon_list, trait_dat)
+    
+    write.csv(trait_dat, file=paste0("data/processed/community_trait_files/traits_", tax_scales[i], "_", spat_scales[j], ".csv"), row.names=F)
+
+    # pull out the nisp data for all the taxa in the focal assemblage ----
+    
+    # match back the Species name with RevisedName
+    RevisedName_list <- dat$RevisedName[match(taxon_list, dat$Species)]
+    matched_nisp <- mammals_nisp[match(RevisedName_list, mammals_nisp$RevisedName),]
+    total_nisp <- colSums(matched_nisp[,colsForBoxes])
+    
+    # calculate weighted trait averages ----
+    
+    # for each trait, first remove the taxa with no trait data
+    traits <- colnames(trait_dat)[-1]
+    
+    trait_weighted_mean <- as.data.frame(matrix(NA, nrow=length(traits), ncol=length(colsForBoxes)))
+    colnames(trait_weighted_mean) <- colnames(matched_nisp)[colsForBoxes]
+    rownames(trait_weighted_mean) <- traits
+    
+    for (t in 1:length(traits)){
+      trait_weighted_mean[t,] <- colSums(matched_nisp[,colsForBoxes]*trait_dat[,t+1], na.rm=T)/total_nisp
+      ### NATE: once you deal with M. frenata, remove the ", na.rm=T" from this line of code!
+    }
+    
+    rownames(trait_weighted_mean) <- traits
+    
+    write.csv(trait_weighted_mean, file=paste0("data/processed/assemblage_trait_mean_files/trait_weighted_means_", tax_scales[i], "_", spat_scales[j], ".csv"), row.names=T)
+    
+  }
 }
+
+
 
 
