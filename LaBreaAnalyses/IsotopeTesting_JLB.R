4b6421947989bec8ac58bd8c60abff16fcc1124e
jessicablois <jblois@ucmerced.edu>
2021-07-22 08:01:00 -0700

    Isotope code
    Refamiliarized myself with code
    Started on a new script, to create a "final" isotope testing script.
    Created a weighted d180 and weighted age for each specimen
    Cleaned up sensitivity analysis
    Compared weighted age to set of values from sensitivity analysis
    Everything below that set of code (lines 178+) has not been revisited

--- LaBreaAnalyses/IsotopeTesting_JLB.R
+++ LaBreaAnalyses/IsotopeTesting_JLB.R
@@ -1,123 +1,58 @@
+library(dplyr)
+library(stringr)
 
-# import isotope data
-isoDat <- read.delim('data/processed/master_isotopes_file.txt', header=T, sep="\t")
+# Import and Clean data ----
+## import isotope data ----
+# isoDat <- read.delim('data/processed/master_isotopes_file.txt', header=T, sep="\t") # this is not used in script
 isoDat2 <- read.csv('data/processed/SIBER/SIBER_raw.csv', header=T)
+# match isotope file to ages file
+samples <- paste("UCIAMS", isoDat2$UCIAMS_Number) # this is the final set of samples with isotope data
 
-# read in calibrated ages
+## read in climate Data ----
+climDat<- read.delim("data/raw/climate/hendy2002data.txt")
+
+## read in calibrated ages ----
 allAges<- read.csv('output/OxCal/final oxcal models/AllAges_ages_probs.csv', header=T)
 
-#remove repetitive information and duplicate samples
-#Jessica - I know lines 12-34 are redundant for your model, 
-#I did it to compare "mean.d18O" and "matchedDF" datasets starting at line 193
-#Im sure there's a more elegant way to do this, but I don't know how
-allAges<-allAges[!(allAges$name=="R Combine 1 LACMP23-33228" | 
-    allAges$name=="R Combine 2 LACMHC-142773" |
-    allAges$name=="R Combine 3 LACMHC-142779" |
-    allAges$name=="R Combine 4 LACMP23-35541" |
-    allAges$name=="R Combine 5 LACMP23-40642" |
-    allAges$name=="UCIAMS 198206" | allAges$name=="UCIAMS 223585" |
-    allAges$name=="UCIAMS 217076" | allAges$name=="UCIAMS 217077" |  
-    allAges$name=="UCIAMS 223587"),]
+## data cleaning on allAges file----
+allAges<- allAges[allAges$probability != 0, ] # remove age estimates with 0 probability
+
+#remove repetitive information and duplicate samples 
+allAges<- allAges[!(allAges$name=="R Combine 1 LACMP23-33228" | 
+                     allAges$name=="R Combine 2 LACMHC-142773" |
+                     allAges$name=="R Combine 3 LACMHC-142779" |
+                     allAges$name=="R Combine 4 LACMP23-35541" |
+                     allAges$name=="R Combine 5 LACMP23-40642" |
+                     allAges$name=="UCIAMS 198206" | allAges$name=="UCIAMS 223585" |
+                     allAges$name=="UCIAMS 217076" | allAges$name=="UCIAMS 217077" |  
+                     allAges$name=="UCIAMS 223587"),]
 
 #remove specimens that are not rabbits and squirrels
-allAges<-allAges[!(allAges$name=="UCIAMS 198199" | 
-    allAges$name=="UCIAMS 198200" | allAges$name=="UCIAMS 198208" |
-    allAges$name=="UCIAMS 216782" | allAges$name=="UCIAMS 223520" |
-    allAges$name=="UCIAMS 198297"),]
+allAges<- allAges[!(allAges$name=="UCIAMS 198199" | 
+                     allAges$name=="UCIAMS 198200" | allAges$name=="UCIAMS 198208" |
+                     allAges$name=="UCIAMS 216782" | allAges$name=="UCIAMS 223520" |
+                     allAges$name=="UCIAMS 198297"),]
 
 #remove specimens without stable isotope values
-allAges<-allAges[!(allAges$name=="UCIAMS 223515" | 
-    allAges$name=="UCIAMS 223519" | allAges$name=="UCIAMS 223522"),]
+allAges<- allAges[!(allAges$name=="UCIAMS 223515" | 
+                     allAges$name=="UCIAMS 223519" | allAges$name=="UCIAMS 223522"),]
 
 #remove specimens with unmeasureable dates
-allAges<-allAges[!(allAges$name=="UCIAMS 223515" | 
-    allAges$name=="UCIAMS 223519" | allAges$name=="UCIAMS 223522"),]
-
-# match isotope file to ages file
-samples <- paste("UCIAMS", isoDat2$UCIAMS_Number)
-
-# Start of test script - commented out for now through line 76----
-# matches <- match(samples, allAges$name)
-# 
-# # remove the unmatched specimen from the files
-# # added ifelse statement to catch instances where there are or are not matches
-# if (length(which(is.na(matches)))>0){
-#   isoDat2_final <- isoDat2[-which(is.na(matches)),]
-#   samples_final <- samples[-which(is.na(matches))]
-# }else{
-#   isoDat2_final <- isoDat2
-#   samples_final <- samples
-# }
-# 
-# N=100
-# N_model_res <- as.data.frame(matrix(data=NA, nrow=N, ncol=6))
-# C_model_res<- as.data.frame(matrix(data=NA, nrow=N, ncol=6))
-# colnames(N_model_res) <- colnames(C_model_res) <- c('Fstat', 'lm_pVal', 'coeff', 'AdjR2', 'cor', 'cor_pVal')
-# 
-# for (k in 1:N){
-# 
-# # for each specimen, sample an age
-# sampledAges <- vector(mode="numeric", length=length(samples_final))
-# 
-#   for (i in 1:length(samples_final)){
-#     # pull out specimen ID
-#     spec <- samples_final[i] 
-#     # pull out all calibrated ages and probabilities
-#     specAges <- allAges[which(allAges$name == spec),]
-#     # sample a single age, with sampling weighted per the probability
-#     sampledAges[i] <- sample(specAges$value, 1, prob=specAges$probability)
-#   }
-#   
-#   # match with isotope data
-#   matchedDF <- cbind(isoDat2_final[,c('UCIAMS_Number', 'Taxon', 'del15N_permil', 'del13C_permil', 'X14C_age_BP')], sampledAges)
-#   
-#   N_model <- lm(del15N_permil ~ sampledAges, data=matchedDF)
-#   C_model <- lm(del13C_permil ~ sampledAges, data=matchedDF)
-#   
-#   par(mfrow=c(1,2))
-#   plot(del15N_permil ~ sampledAges, data=matchedDF, pch=16)
-#   abline(N_model)
-#   plot(del13C_permil ~ sampledAges, data=matchedDF, pch=16)
-#   abline(C_model)
-# 
-#   N_cor.test <- cor.test(matchedDF$del15N_permil, matchedDF$sampledAges)
-#   C_cor.test <- cor.test(matchedDF$del13C_permil, matchedDF$sampledAges)
-#   
-#   N_model_res[k, 1]<- summary(N_model)$fstatistic[1]
-#   N_model_res[k, 2]<- summary(N_model)$coefficients[8]
-#   N_model_res[k, 3]<- summary(N_model)$coefficients[2]
-#   N_model_res[k, 4]<- summary(N_model)$adj.r.squared
-#   N_model_res[k, 5]<- N_cor.test$estimate
-#   N_model_res[k, 6]<- N_cor.test$p.value
-#   
-#   C_model_res[k, 1]<- summary(C_model)$fstatistic[1]
-#   C_model_res[k, 2]<- summary(C_model)$coefficients[8]
-#   C_model_res[k, 3]<- summary(C_model)$coefficients[2]
-#   C_model_res[k, 4]<- summary(C_model)$adj.r.squared
-#   C_model_res[k, 5]<- C_cor.test$estimate
-#   C_model_res[k, 6]<- C_cor.test$p.value
-#   
-# }
-# 
-# hist(N_model_res$cor, xlim=c(-0.1,0.1))
-# hist(C_model_res$cor, xlim=c(0.3,0.45))
+allAges<- allAges[!(allAges$name=="UCIAMS 223515" | 
+                     allAges$name=="UCIAMS 223519" | allAges$name=="UCIAMS 223522"),]
 
 
-# Start of Nate's script. This is the final one that is being modified ----
-# this script does the sampling and then calculates the correlation with the ages
-# but...you probably want to add a step where you match the sampledAges with the d18O climate curve, and then calculate the correlation
-
-dat2<- read.delim("data/raw/climate/hendy2002data.txt")
-
+# Match d180 to all age estimates for every sample ----
+# end result is a new dataframe, adding d18O to allAges
+all(allAges$value<0) # this should be true, then take the absolute values to match to climate
 xout <- abs(allAges$value)
-Hendy_extracted <- approx(x=dat2$HendyAge, y=dat2$pach.d18O, method="linear", xout=xout)
+Hendy_extracted <- approx(x=climDat$HendyAge, y=climDat$pach.d18O, method="linear", xout=xout)
 d18O<-Hendy_extracted$y
-allAges_d18O<-cbind(allAges, d18O)
-allAges_d18O<-allAges_d18O[allAges_d18O$probability != 0, ]
+allAges_d18O<-cbind(allAges, d18O) # this matches d180 to all the age estimates, for all probabilities
 
+# remove any unmatched specimen from the files
 matches <- match(samples, allAges_d18O$name)
 
-# remove the unmatched specimen from the files
 # added ifelse statement to catch instances where there are or are not matches
 if (length(which(is.na(matches)))>0){
   isoDat2_final <- isoDat2[-which(is.na(matches)),]
@@ -126,7 +61,48 @@ if (length(which(is.na(matches)))>0){
   isoDat2_final <- isoDat2
   samples_final <- samples
 }
+# end result dataframes:
+# isoDat2_final - final C/N isotope data
+# samples_final - final set of samples
+# allAges_d18O - ages, probabilities, and d18O for each sample
 
+
+# Primary analysis: calculate weighted mean d18O and weighted age for each specimen ----
+# For each specimen, calculate the weighted d180 and weighted mean age, based on the probability of each calibrated age from the age models, then do the linear models
+
+specimen_wd18O <- vector(mode="numeric", length=length(samples_final))
+specimen_wage <- vector(mode="numeric", length=length(samples_final))
+
+for (i in 1:length(samples_final)){
+  # pull out specimen ID
+  spec <- samples_final[i] 
+  # pull out all calibrated ages and probabilities for that single specimen
+  specd18O <- allAges_d18O[which(allAges_d18O$name == spec),]
+  # calculate weighted mean d18O
+  specimen_wd18O[i] <- weighted.mean(specd18O$d18O, specd18O$probability)
+  # calculate weighted mean age
+  specimen_wage[i] <- weighted.mean(specd18O$value, specd18O$probability)
+}
+
+# match with isotope data
+matchedDF_weighted <- cbind(isoDat2_final[,c('UCIAMS_Number', 'Taxon', 'del15N_permil', 'del13C_permil', 'X14C_age_BP')], specimen_wd18O, specimen_wage)
+
+N_model_weighted <- lm(del15N_permil ~ specimen_wd18O, data=matchedDF_weighted)
+summary(N_model_weighted)
+C_model_weighted <- lm(del13C_permil ~ specimen_wd18O, data=matchedDF_weighted)
+summary(C_model_weighted)
+
+par(mfrow=c(1,2))
+plot(del15N_permil ~ specimen_wd18O, data=matchedDF_weighted, pch=16)
+abline(N_model_weighted)
+plot(del13C_permil ~ specimen_wd18O, data=matchedDF_weighted, pch=16)
+abline(C_model_weighted)
+
+N_cor.test_weighted <- cor.test(matchedDF_weighted$del15N_permil, matchedDF_weighted$specimen_wd18O)
+C_cor.test_weighted <- cor.test(matchedDF_weighted$del13C_permil, matchedDF_weighted$specimen_wd18O)
+
+
+# Sensitivity analysis  ----
 N=100 # Note: some specimens do not have 100 age estimates.
 N_model_res <- as.data.frame(matrix(data=NA, nrow=N, ncol=6))
 C_model_res<- as.data.frame(matrix(data=NA, nrow=N, ncol=6))
@@ -147,55 +123,61 @@ for (k in 1:N){
   }
   
   # match with isotope data
-  matchedDF <- cbind(isoDat2_final[,c('UCIAMS_Number', 'Taxon', 'del15N_permil', 'del13C_permil', 'X14C_age_BP')], sampledd18O)
+  matchedDF_sensitivity <- cbind(isoDat2_final[,c('UCIAMS_Number', 'Taxon', 'del15N_permil', 'del13C_permil', 'X14C_age_BP')], sampledd18O)
   #matchedDF <- cbind(matchedDF, weighted.d18O) ## NATE - No 'weighted.d18O' object found, commented this out.
   
-  N_model <- lm(del15N_permil ~ sampledd18O, data=matchedDF)
-  C_model <- lm(del13C_permil ~ sampledd18O, data=matchedDF)
+  N_model_sensitivity <- lm(del15N_permil ~ sampledd18O, data=matchedDF_sensitivity)
+  C_model_sensitivity <- lm(del13C_permil ~ sampledd18O, data=matchedDF_sensitivity)
   
   par(mfrow=c(1,2))
-  plot(del15N_permil ~ sampledd18O, data=matchedDF, pch=16)
-  abline(N_model)
-  plot(del13C_permil ~ sampledd18O, data=matchedDF, pch=16)
-  abline(C_model)
+  plot(del15N_permil ~ sampledd18O, data=matchedDF_sensitivity, pch=16)
+  abline(N_model_sensitivity)
+  plot(del13C_permil ~ sampledd18O, data=matchedDF_sensitivity, pch=16)
+  abline(C_model_sensitivity)
   
-  N_cor.test <- cor.test(matchedDF$del15N_permil, matchedDF$sampledd18O)
-  C_cor.test <- cor.test(matchedDF$del13C_permil, matchedDF$sampledd18O)
+  N_cor.test_sensitivity <- cor.test(matchedDF_sensitivity$del15N_permil, matchedDF_sensitivity$sampledd18O)
+  C_cor.test_sensitivity <- cor.test(matchedDF_sensitivity$del13C_permil, matchedDF_sensitivity$sampledd18O)
   
-  N_model_res[k, 1]<- summary(N_model)$fstatistic[1]
-  N_model_res[k, 2]<- summary(N_model)$coefficients[8]
-  N_model_res[k, 3]<- summary(N_model)$coefficients[2]
-  N_model_res[k, 4]<- summary(N_model)$adj.r.squared
-  N_model_res[k, 5]<- N_cor.test$estimate
-  N_model_res[k, 6]<- N_cor.test$p.value
+  N_model_res[k, 1]<- summary(N_model_sensitivity)$fstatistic[1]
+  N_model_res[k, 2]<- summary(N_model_sensitivity)$coefficients[8]
+  N_model_res[k, 3]<- summary(N_model_sensitivity)$coefficients[2]
+  N_model_res[k, 4]<- summary(N_model_sensitivity)$adj.r.squared
+  N_model_res[k, 5]<- N_cor.test_sensitivity$estimate
+  N_model_res[k, 6]<- N_cor.test_sensitivity$p.value
   
-  C_model_res[k, 1]<- summary(C_model)$fstatistic[1]
-  C_model_res[k, 2]<- summary(C_model)$coefficients[8]
-  C_model_res[k, 3]<- summary(C_model)$coefficients[2]
-  C_model_res[k, 4]<- summary(C_model)$adj.r.squared
-  C_model_res[k, 5]<- C_cor.test$estimate
-  C_model_res[k, 6]<- C_cor.test$p.value
+  C_model_res[k, 1]<- summary(C_model_sensitivity)$fstatistic[1]
+  C_model_res[k, 2]<- summary(C_model_sensitivity)$coefficients[8]
+  C_model_res[k, 3]<- summary(C_model_sensitivity)$coefficients[2]
+  C_model_res[k, 4]<- summary(C_model_sensitivity)$adj.r.squared
+  C_model_res[k, 5]<- C_cor.test_sensitivity$estimate
+  C_model_res[k, 6]<- C_cor.test_sensitivity$p.value
   
 }
-
-hist(N_model_res$cor)
-hist(C_model_res$cor)
 
 #Jessica - Can we run many iterations on the model above (e.g., 1000)
 #and use the min of mins and max of maxes below to better quantify 
 #the sensitivity of these estimates and the full range of R2 values?
+# sure: that's what the script above does (though only for N=100), then the apply function below shows a variety of summary stats
 
-min(C_model_res$AdjR2)
-max(C_model_res$AdjR2)
+# Calculate summary statistics on each column
+apply(N_model_res, 2, summary)
+apply(C_model_res, 2, summary)
+
+# Supplemental figure for appendix - sensitivity analysis ----
+## Compare final estimate with sensitivity analysis ----
+
+par(mfrow=c(1,2))
+hist(N_model_res$cor, xlab="N~O correlation")
+segments(N_cor.test_weighted$estimate, 0, N_cor.test_weighted$estimate, 17, 
+         col="red", lwd=2)
+hist(C_model_res$cor, xlab="C~O correlation")
+segments(C_cor.test_weighted$estimate, 0, C_cor.test_weighted$estimate, 25, 
+         col="red", lwd=2)
 
 
-#extract mean d18O value across all ages for per specimen average - code needs to be fixed
+# Original analysis, naive mean, no weighting ____
+# extract mean d18O value across all ages for per specimen average - code needs to be fixed
 mean.d18O<-aggregate( d18O ~ name, allAges_d18O, mean )
-
-#merge(matchedDF, mean.d18O, by=number, all=TRUE)
-#
-library(dplyr)
-library(stringr)
 
 #Remove "UCIAMS" from name column 
 mean.d18O <- mean.d18O %>%
