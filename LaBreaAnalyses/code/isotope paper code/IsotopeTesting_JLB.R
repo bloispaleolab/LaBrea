28a0d679f4e4a795028bbc88c658b5483e247387
jessicablois <jblois@ucmerced.edu>
2021-11-02 08:21:48 -0700

    Worked on adding ngrip data to code and making comparisons
    not finished yet, initial takeaway is that may not matter much?
    need to get the plotting script right - for now, just put plots into an "if" statement so not overwriting hendy data plots.
    started to add a script for ngrip in the Figure 4 section but commented out for now.

--- LaBreaAnalyses/code/isotope paper code/IsotopeTesting_JLB.R
+++ LaBreaAnalyses/code/isotope paper code/IsotopeTesting_JLB.R
@@ -218,124 +218,138 @@ summary(carbon.lm.all.age)
 nitrogen.lm.all.age<-step(lm(del15N_permil ~ specimen_mediand18O * Taxon * specimen_medianage, data=matchedDF_weighted))
 summary(nitrogen.lm.all.age)
 
+
+
 # NEW final plot ####
-## Figure 3 ----
 
-#grDevices::pdf("output/Figure3_lm_carbon_nitrogen_all_July2021_NF.pdf", width=8, height=8)
-grDevices::cairo_pdf("output/Figure3_lm_carbon_nitrogen_all_Aug2021_JB.pdf", width=8, height=8)
+if (climFile=="hendy"){
+  ## Figure 3 ----
+  
+  #grDevices::pdf("output/Figure3_lm_carbon_nitrogen_all_July2021_NF.pdf", width=8, height=8)
+  #grDevices::cairo_pdf("output/Figure3_lm_carbon_nitrogen_all_Aug2021_JB.pdf", width=8, height=8)
+  
+  layout(matrix(seq(1:6), ncol=3, nrow=2, byrow=F), widths=c(2.5,2.5,1))
+  par(mar=c(4,4,1,1), cex.axis=1, bty="l")
+  
+  
+  # carbon
+  plot(del13C_permil~specimen_mediand18O, data=matchedDF_weighted, pch=16, type="n", xlab="", ylab="")
+  points(del13C_permil~specimen_mediand18O, 
+         data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Sylvilagus"),], 
+         pch=16, col="darkorange")
+  points(del13C_permil~specimen_mediand18O, 
+         data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Otospermophilus"),], 
+         pch=16, col="royalblue2")
+  abline(carbon.lm.final, lty=2)
+  abline(carbon.lm.clim, lty=1)
+  mtext(expression({delta}^18*O~'\u2030'), side=1, line=2.25) 
+  mtext(expression({delta}^13*C~'\u2030'), side=2, line=2)
+  
+  boxplot(carbon.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], 
+          xlab="", ylab="")
+  stripchart(carbon.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], vertical=TRUE, add=TRUE, method="stack", col=c("royalblue2","darkorange"), pch=16)
+  mtext("Taxon", side=1, line=2.25)
+  mtext(expression('Residuals (Climate-only Model)'), side=2, line=2.25, cex=0.8)
+  # mtext(expression('Residuals ('~{delta}^13*C~'\u2030'~' ~ '~{delta}^18*O~'\u2030'~')'), side=2, line=2.25, cex=0.8)
+  legend("topright", legend=paste0("t=", round(c.t.clim$statistic,2), "; df=", round(c.t.clim$parameter,2), "; p=", round(c.t.clim$p.value,2)), bty = "n", cex = 0.8)
+  
+  # nitrogen
+  plot(del15N_permil~specimen_mediand18O, data=matchedDF_weighted, pch=16, 
+       xlab = "", ylab = "", type="n")
+  points(del15N_permil~specimen_mediand18O, 
+         data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Sylvilagus"),], 
+         pch=16, col="darkorange")
+  points(del15N_permil~specimen_mediand18O, 
+         data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Otospermophilus"),], 
+         pch=16, col="royalblue2")
+  abline(nitrogen.lm.final, lty=2)
+  abline(nitrogen.lm.clim, lty=1)
+  mtext(expression({delta}^18*O~'\u2030'), side=1, line=2.25)
+  mtext(expression({delta}^15*N~'\u2030'), side=2, line=2.25)
+  
+  boxplot(nitrogen.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))],
+          xlab="", ylab="")
+  stripchart(nitrogen.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], vertical=TRUE, add=TRUE, method="stack", col=c("royalblue2","darkorange"), pch=16)
+  mtext("Taxon", side=1, line=2.25)
+  mtext(expression('Residuals (Climate-only Model)'), side=2, line=2.25, cex=0.8)
+  # mtext(expression('Residuals ('~{delta}^15*N~'\u2030'~' ~ '~{delta}^18*O~'\u2030'~')'), side=2, line=2.25, cex=0.8)
+  legend("topright", legend=paste0("t=", round(n.t.clim$statistic,2), "; df=", round(n.t.clim$parameter,2), "; p=", round(n.t.clim$p.value,2)), bty = "n", cex = 0.8)
+  
+  # taxon legend
+  # Draw an empty plot
+  plot(5, 5, 
+       type="n", axes=FALSE, ann=FALSE, 
+       xlim=c(0, 10), ylim = c(0,10))
+  legend(xpd=T, x=-5, y=5, 
+         legend = c("Otospermophilus", "Sylvilagus"),
+         title="Taxon",
+         col = c("royalblue2","darkorange"), pch = 16,
+         bty = "n", cex = 0.8)
+  
+  # model legend
+  plot(5, 5, 
+       type="n", axes=FALSE, ann=FALSE, 
+       xlim=c(0, 10), ylim = c(0,10))
+  legend(xpd=T, x=-5, y=5,
+         legend = c("Climate+Taxon", "Climate-only"),
+         title="Model",
+         lty = c(2,1),
+         bty = "n", cex = 0.8)
+  
+  dev.off()
+  
+  # I haven't put A-D labels on them yet.
+  # Figure caption text.
+  # Figure 3. The relationship between isotope niche and climate. A) & C) show the relationship between 13C or 15N, respectively, and 18O.  In both panels, the dashed line indicates the fitted relationship between 13C or 15N and 18O from the final model which  includes Taxon as an independent variable. The solid line indicates the fitted relationship between 13C or 15N and 18O from a linear model that just includes 18O as the independent variable. B) & D) indicate the residuals from the climate-only model, plotted by taxon.
+  
+  ## Figure 4 ----
+  #plot carbon and climate thru time
+  
+  # order matchedDF_weighted
+  matchedDF_weighted <- matchedDF_weighted[order(matchedDF_weighted$specimen_medianage),]
+  
+  #grDevices::pdf(file="output/Figure4_carbon_climate_time_updated_NF.pdf", height=6, width=8)
+  #grDevices::cairo_pdf(file="output/Figure4_carbon_climate_time_updated_JB.pdf", height=6, width=8)
+  
+  layout(matrix(seq(1:2), ncol=1, nrow=2), heights=c(0.75,1))
+  par(mar=c(4,5,0,5), cex.axis=1, bty="l", xpd=F)
+  
+  # d13C plot
+  plot(del13C_permil~specimen_medianage, data=matchedDF_weighted, 
+       type="n", axes=FALSE, ann=FALSE, xaxs="i", yaxs="r", 
+       xlim=c(55000, 0))
+  axis(4)
+  mtext(expression({delta}^13*C~'\u2030'), side=4, line=2.25)
+  lines(del13C_permil~specimen_medianage, data=matchedDF_weighted, lty=1, col="brown")
+  points(del13C_permil~specimen_medianage, data=matchedDF_weighted, pch=16, col="brown", cex=0.5)
+  
+  #d18O plot - Hendy
+  plot(pach.d18O~HendyAge, dat=climDat,
+       type="l",
+       xlab="Years before present",
+       ylab = expression({delta}^18*O~'\u2030'),
+       bty="n",
+       xlim=c(55000, 0), ylim=c(3, 0),
+       lab=c(12, 8, 7), xaxs="i", yaxs="r", col="gray")
+  x1.05<- loess(climDat$pach.d18O~climDat$HendyAge, span=0.05)
+  lines(x1.05$fitted~x1.05$x, type="l", col="darkgray")
 
-layout(matrix(seq(1:6), ncol=3, nrow=2, byrow=F), widths=c(2.5,2.5,1))
-par(mar=c(4,4,1,1), cex.axis=1, bty="l")
-
-
-# carbon
-plot(del13C_permil~specimen_mediand18O, data=matchedDF_weighted, pch=16, type="n", xlab="", ylab="")
-points(del13C_permil~specimen_mediand18O, 
-       data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Sylvilagus"),], 
-       pch=16, col="darkorange")
-points(del13C_permil~specimen_mediand18O, 
-       data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Otospermophilus"),], 
-       pch=16, col="royalblue2")
-abline(carbon.lm.final, lty=2)
-abline(carbon.lm.clim, lty=1)
-mtext(expression({delta}^18*O~'\u2030'), side=1, line=2.25) 
-mtext(expression({delta}^13*C~'\u2030'), side=2, line=2)
-
-boxplot(carbon.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], 
-        xlab="", ylab="")
-stripchart(carbon.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], vertical=TRUE, add=TRUE, method="stack", col=c("royalblue2","darkorange"), pch=16)
-mtext("Taxon", side=1, line=2.25)
-mtext(expression('Residuals (Climate-only Model)'), side=2, line=2.25, cex=0.8)
-# mtext(expression('Residuals ('~{delta}^13*C~'\u2030'~' ~ '~{delta}^18*O~'\u2030'~')'), side=2, line=2.25, cex=0.8)
-legend("topright", legend=paste0("t=", round(c.t.clim$statistic,2), "; df=", round(c.t.clim$parameter,2), "; p=", round(c.t.clim$p.value,2)), bty = "n", cex = 0.8)
-
-# nitrogen
-plot(del15N_permil~specimen_mediand18O, data=matchedDF_weighted, pch=16, 
-     xlab = "", ylab = "", type="n")
-points(del15N_permil~specimen_mediand18O, 
-       data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Sylvilagus"),], 
-       pch=16, col="darkorange")
-points(del15N_permil~specimen_mediand18O, 
-       data=matchedDF_weighted[which(matchedDF_weighted$Taxon == "Otospermophilus"),], 
-       pch=16, col="royalblue2")
-abline(nitrogen.lm.final, lty=2)
-abline(nitrogen.lm.clim, lty=1)
-mtext(expression({delta}^18*O~'\u2030'), side=1, line=2.25)
-mtext(expression({delta}^15*N~'\u2030'), side=2, line=2.25)
-
-boxplot(nitrogen.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))],
-        xlab="", ylab="")
-stripchart(nitrogen.lm.clim$residuals ~ matchedDF_weighted$Taxon[which(!is.na(matchedDF_weighted$specimen_mediand18O))], vertical=TRUE, add=TRUE, method="stack", col=c("royalblue2","darkorange"), pch=16)
-mtext("Taxon", side=1, line=2.25)
-mtext(expression('Residuals (Climate-only Model)'), side=2, line=2.25, cex=0.8)
-# mtext(expression('Residuals ('~{delta}^15*N~'\u2030'~' ~ '~{delta}^18*O~'\u2030'~')'), side=2, line=2.25, cex=0.8)
-legend("topright", legend=paste0("t=", round(n.t.clim$statistic,2), "; df=", round(n.t.clim$parameter,2), "; p=", round(n.t.clim$p.value,2)), bty = "n", cex = 0.8)
-
-# taxon legend
-# Draw an empty plot
-plot(5, 5, 
-     type="n", axes=FALSE, ann=FALSE, 
-     xlim=c(0, 10), ylim = c(0,10))
-legend(xpd=T, x=-5, y=5, 
-       legend = c("Otospermophilus", "Sylvilagus"),
-       title="Taxon",
-       col = c("royalblue2","darkorange"), pch = 16,
-       bty = "n", cex = 0.8)
-
-# model legend
-plot(5, 5, 
-     type="n", axes=FALSE, ann=FALSE, 
-     xlim=c(0, 10), ylim = c(0,10))
-legend(xpd=T, x=-5, y=5,
-       legend = c("Climate+Taxon", "Climate-only"),
-       title="Model",
-       lty = c(2,1),
-       bty = "n", cex = 0.8)
-
-dev.off()
-
-# I haven't put A-D labels on them yet.
-# Figure caption text.
-# Figure 3. The relationship between isotope niche and climate. A) & C) show the relationship between 13C or 15N, respectively, and 18O.  In both panels, the dashed line indicates the fitted relationship between 13C or 15N and 18O from the final model which  includes Taxon as an independent variable. The solid line indicates the fitted relationship between 13C or 15N and 18O from a linear model that just includes 18O as the independent variable. B) & D) indicate the residuals from the climate-only model, plotted by taxon.
-
-## Figure 4 ----
-#plot carbon and climate thru time
-
-# order matchedDF_weighted
-matchedDF_weighted <- matchedDF_weighted[order(matchedDF_weighted$specimen_medianage),]
-
-grDevices::pdf(file="output/Figure4_carbon_climate_time_updated_NF.pdf", height=6, width=8)
-#grDevices::cairo_pdf(file="output/Figure4_carbon_climate_time_updated_JB.pdf", height=6, width=8)
-
-layout(matrix(seq(1:2), ncol=1, nrow=2), heights=c(0.75,1))
-par(mar=c(4,5,0,5), cex.axis=1, bty="l", xpd=F)
-
-# d13C plot
-plot(del13C_permil~specimen_medianage, data=matchedDF_weighted, 
-     type="n", axes=FALSE, ann=FALSE, xaxs="i", yaxs="r", 
-     xlim=c(55000, 0))
-axis(4)
-mtext(expression({delta}^13*C~'\u2030'), side=4, line=2.25)
-lines(del13C_permil~specimen_medianage, data=matchedDF_weighted, lty=1, col="brown")
-points(del13C_permil~specimen_medianage, data=matchedDF_weighted, pch=16, col="brown", cex=0.5)
-
-#d18O plot
-plot(pach.d18O~HendyAge, dat=climDat, 
-     type="l", 
-     xlab="Years before present", 
-     ylab = expression({delta}^18*O~'\u2030'),
-     bty="n", 
-     xlim=c(55000, 0), ylim=c(3, 0),
-     lab=c(12, 8, 7), xaxs="i", yaxs="r", col="gray")
-x1.05<- loess(climDat$pach.d18O~climDat$HendyAge, span=0.05)
-lines(x1.05$fitted~x1.05$x, type="l", col="darkgray")
-
-# add symbols for d18O at times with isotopes
-lines(specimen_mediand18O~specimen_medianage, data=matchedDF_weighted, lty=1, col="blue", cex=0.5)
-
-points(specimen_mediand18O~specimen_medianage, data=matchedDF_weighted, pch=16, col="blue", cex=0.5)
-dev.off()
-
+  #d18O plot - ngrip
+  # plot(d18O~Age, dat=climDat, 
+  #      type="l", 
+  #      xlab="Years before present", 
+  #      ylab = expression({delta}^18*O~'\u2030'),
+  #      bty="n", 
+  #      xlim=c(55000, 0), ylim=c(-45, -32),
+  #      lab=c(12, 8, 7), xaxs="i", yaxs="r", col="gray")
+  # x1.05<- loess(climDat$d18O~climDat$Age, span=0.05)
+  # lines(x1.05$fitted~x1.05$x, type="l", col="darkgray")
+  # # add symbols for d18O at times with isotopes
+  # lines(specimen_mediand18O~specimen_medianage, data=matchedDF_weighted, lty=1, col="blue", cex=0.5)
+  
+  points(specimen_mediand18O~specimen_medianage, data=matchedDF_weighted, pch=16, col="blue", cex=0.5)
+  dev.off()
+}
 # Sensitivity analysis  ----
 # sensitivity analysis #1: compare weighted and median age models, using the final models from the primary analysis
 
